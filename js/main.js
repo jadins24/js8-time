84b8d42407e2b6c2348a8c7883fea82c8ab9f521
jadins24 <jadinesh04@gmail.com>
2022-12-27 16:54:09 +0530

    add

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var a=new Date();
+var b=a.setTime(parseInt(prompt("Enter the time")));
+if(b>=0 && b<=5){
+  console.log("Early Morning")
+}
+else if(b>=6 && b<=11){
+  console.log("Morning")
+}
+else if(b>=12 && b<=15){
+  console.log("Afternoon")
+}
+else if(b>=16 && b<=18){
+  console.log("Evening")
+}
+else if(b>=19 && b<24){
+  console.log("Night")
+}
